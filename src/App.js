0536c82252054b6c6b78ba4ada3d6e1d95c23df7
mihirps <mihirsabhaya99@gmail.com>
2021-09-29 21:59:18 +0530

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './style.css';
+import Navbar from "./Navbar.js"
 
 export default function App() {
   return (
+
+    <>
+    <Navbar />
     <div>
       <h1 className="text-center">Hello StackBlitz!</h1>
     </div>
+    </>
   );
 }
